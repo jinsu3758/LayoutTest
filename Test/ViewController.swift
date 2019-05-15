929f5e872f49493c71755b4bc31811b2627667a0
jinsu3758 <jinsu3758@naver.com>
2019-05-15 15:49:41 +0900

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by 박진수 on 2019. 5. 15..
+//  Copyright © 2019년 Jinsu. Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
